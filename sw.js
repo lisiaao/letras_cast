b010219cd6e49ada3ceb52d42e6dc88c7a684435
lisiaao <lisia.andoli@gmail.com>
2026-08-18 11:21:00 -0300

    Update service worker cache version and add activation
    Updated cache name and added activation event listener to handle old caches.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'letras-cast-v2';
+const CACHE_NAME = 'letras-cast-v3';
 const ASSETS = [
   '/',
   '/index.html',
@@ -6,7 +6,22 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
